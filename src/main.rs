85ee83ad88d14d094c53377b40cf58c2c189f75d
Colton J. McCurdy <mccurdyc22@gmail.com>
2021-10-25 21:06:31 -0400

    proper filtering of tags with missing ids or purchases
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,40 @@
 use std::env;
 
 mod airtable;
-use crate::airtable::Airtable;
+use crate::airtable::{tags::Tag, Airtable};
 
 #[tokio::main]
 async fn main() {
     let api_token = env::var("AIRTABLE_API_TOKEN").unwrap();
     let base_id = env::var("AIRTABLE_BASE_ID").unwrap();
 
-    if let Ok(t) = Airtable::new(&api_token, &base_id) {
-        let tags = t.get_tags().await;
+    if let Ok(table) = Airtable::new(&api_token, &base_id) {
+        let tags = table.get_tags().await;
         match tags {
             Ok(tags) => {
-                println!("{:?}", tags);
+                println!(
+                    "{:?}",
+                    tags.records
+                        .into_iter()
+                        .filter(|t| t.id.is_some() && t.fields.purchases.is_some())
+                        .collect::<Vec<Tag>>()
+                );
             }
             Err(err) => {
                 println!("{:?}", err);
             }
         }
 
-        let merchants = t.get_merchants().await;
-        match merchants {
-            Ok(merchants) => {
-                println!("{:?}", merchants);
-            }
-            Err(err) => {
-                println!("{:?}", err);
-            }
-        }
-
+        // let merchants = t.get_merchants().await;
+        // match merchants {
+        //     Ok(merchants) => {
+        //         println!("{:?}", merchants);
+        //     }
+        //     Err(err) => {
+        //         println!("{:?}", err);
+        //     }
+        // }
+        //
         // let purchases = t.get_purchases().await;
         // match purchases {
         //     Ok(purchases) => {
