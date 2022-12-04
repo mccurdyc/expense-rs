a59120aac3e11266efad7121e6ac4d9d225b33ca
Colton J. McCurdy <mccurdyc22@gmail.com>
2022-12-03 23:24:57 -0500

    devenv shell working
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- devenv.nix
+++ devenv.nix
@@ -1,26 +1,11 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 
+let
+  pkgs-stable = import inputs.nixpkgs-stable { inherit (pkgs.system); };
+in
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
-
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
-
-  enterShell = ''
-    hello
-    git --version
-  '';
-
-  # https://devenv.sh/languages/
-  languages.nix.enable = true;
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
-
-  # https://devenv.sh/pre-commit-hooks/
-  pre-commit.hooks.shellcheck.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
+  packages = [
+    pkgs-stable.git
+    pkgs-stable.gh
+  ];
 }
